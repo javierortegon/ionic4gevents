07a6150f3f4c066e40b870e1e3df59191458d44a
javierortegon <javierortegonmp@gmail.com>
2019-09-24 09:51:49 -0500

    login exitoso con facebook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { ComponentsModule } from './components/components.module';
-
 import { ServiceWorkerModule } from '@angular/service-worker';
-
 import { environment } from '../environments/environment';
 import { ReactiveFormsModule } from '@angular/forms';
-
 import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
-
 import { ModalPreguntaPageModule } from './pages/modal-pregunta/modal-pregunta.module';
+import { Facebook } from '@ionic-native/facebook/ngx';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+
 
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -46,7 +42,9 @@ export function createTranslateLoader(http: HttpClient) {
   ],
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    InAppBrowser
+    InAppBrowser,
+    Facebook,
+    NativeStorage
   ],
  
   bootstrap: [AppComponent]
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -2,6 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
+import { Facebook } from '@ionic-native/facebook/ngx';
+import { NativeStorage } from '@ionic-native/native-storage/ngx';
+import { LoadingController } from '@ionic/angular';
+import { Platform } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-login',
@@ -11,6 +17,8 @@ import { MenuController } from '@ionic/angular';
   ]
 })
 export class LoginPage implements OnInit {
+  FB_APP_ID: number = 2328564010739918;
+
   loginForm: FormGroup;
 
   validation_messages = {
@@ -26,6 +34,11 @@ export class LoginPage implements OnInit {
 
   constructor(
     public router: Router,
+    private fb: Facebook,
+		private nativeStorage: NativeStorage,
+		public loadingController: LoadingController,
+		private platform: Platform,
+		public alertController: AlertController,
     public menu: MenuController
   ) {
     this.loginForm = new FormGroup({
@@ -39,6 +52,48 @@ export class LoginPage implements OnInit {
       ]))
     });
   }
+
+  async doFbLogin(){
+		const loading = await this.loadingController.create({
+			message: 'Please wait...'
+		});
+		this.presentLoading(loading);
+
+		//the permissions your facebook app needs from the user
+    const permissions = ["public_profile", "email"];
+
+		this.fb.login(permissions)
+		.then(response =>{
+			let userId = response.authResponse.userID;
+
+			//Getting name and gender properties
+			this.fb.api("/me?fields=name,email", permissions)
+			.then(user =>{
+				user.picture = "https://graph.facebook.com/" + userId + "/picture?type=large";
+				//now we have the users info, let's save it in the NativeStorage
+				this.nativeStorage.setItem('facebook_user',
+				{
+					name: user.name,
+					email: user.email,
+					picture: user.picture
+				})
+				.then(() =>{
+					this.router.navigate(["/user"]);
+					loading.dismiss();
+				}, error =>{
+					console.log(error);
+					loading.dismiss();
+				})
+			})
+		}, error =>{
+			console.log(error);
+			loading.dismiss();
+		});
+	}
+
+	async presentLoading(loading) {
+		return await loading.present();
+	}
 
   ngOnInit(): void {
     this.menu.enable(false);
